0f7d1e9e099a7168c931c2a348cb9780dc075389
xinghuishama <xinghuip@gmail.com>
2026-05-10 12:41:06 +0800

    Delete js directory

--- js/filters.js
+++
@@ -1,7 +0,0 @@
-function buildFilterSets(filters) {
-  const sets = {};
-  for (const k in filters) {
-    sets[k] = new Set(filters[k]);
-  }
-  return sets;
-}
--- js/parser.js
+++
@@ -1,8 +0,0 @@
-function parseInput(input) {
-  return [...new Set(
-    input
-      .split(/\s+/)
-      .map(n => parseInt(n, 10))
-      .filter(n => Number.isInteger(n) && n >= 1 && n <= 49)
-  )];
-}
